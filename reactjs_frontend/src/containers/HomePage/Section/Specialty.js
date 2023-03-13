7a135f593d2462991e25abb0f88560abd56aee17
baothi <baothi246@gmail.com>
2023-03-13 11:45:49 +0700

    layout out chuyen kho pho bien

--- reactjs_frontend/src/containers/HomePage/Section/Specialty.js
+++ reactjs_frontend/src/containers/HomePage/Section/Specialty.js
@@ -6,6 +6,18 @@ import Slider from "react-slick";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import coxuongkhopImg from "../../../assets/specialty/co-xuong-khop.jpg";
+import thankinhImg from "../../../assets/specialty/than-kinh.jpg";
+import chamcuuImg from "../../../assets/specialty/cham-cuu.jpg";
+import cotsongImg from "../../../assets/specialty/cot-song.jpg";
+import dalieuImg from "../../../assets/specialty/da-lieu.jpg";
+import sanphukhoaImg from "../../../assets/specialty/san-phu-khoa.jpg";
+import sieuamImg from "../../../assets/specialty/san-phu-khoa.jpg";
+import taimuihongImg from "../../../assets/specialty/tai-mui-hong.jpg";
+import tieuhoaImg from "../../../assets/specialty/tieu-hoa.jpg";
+import timmachImg from "../../../assets/specialty/tim-mach.jpg";
+import yhoccotruyenImg from "../../../assets/specialty/y-hoc-co-truyen.jpg";
+
 
 class Specialty extends Component {
 
@@ -13,43 +25,71 @@ class Specialty extends Component {
     this.props.changeLanguageAppRedux(language)
   }
 
+
   render() {
     let settings = {
       dots: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 4,
       slideToScroll: 1
     };
     return (
-      <div className='section-specialty'>
-        <div className='specialty-content'>
-          <Slider {...settings}>
-            <div className='img-customize'>
-              <h3>1</h3>
-            </div>
-            <div className='img-customize'>
-              <h3>2</h3>
-            </div>
-            <div className='img-customize'>
-              <h3>3</h3>
-            </div>
-            <div className='img-customize'>
-              <h3>4</h3>
-            </div>
-            <div className='img-customize'>
-              <h3>5</h3>
-            </div>
-            <div className='img-customize'>
-              <h3>6</h3>
-            </div>
-            <div className='img-customize'>
-              <h3>7</h3>
-            </div>
-            <div className='img-customize'>
-              <h3>8</h3>
-            </div>
-          </Slider>
+      <div className="section-specialty">
+        <div className="specialty-container">
+          <div className="specialty-header">
+            <span className="title-section">Chuyên Khoa Phổ Biến</span>
+            <button className="btn-section">Xem Thêm</button>
+          </div>
+          <div className="specialty-body">
+            <Slider {...settings}>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Cơ Xương Khớp</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Thần Kinh</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Châm Cứu</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Cột Sống</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Da Liễu</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Siêu Âm</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Tai Mũi Họng</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Sản Phụ Khoa</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Tiêu Hóa</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Tim Mạch</div>
+              </div>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Y Học Cổ Truyền</div>
+              </div>
+            </Slider>
+          </div>
+
         </div>
       </div>
     );
