485e0d81d0661a5c7d5ef6fe2132cd5349bc391d
baothi <baothi246@gmail.com>
2023-03-17 19:35:14 +0700

    change language

--- reactjs_frontend/src/containers/App.js
+++ reactjs_frontend/src/containers/App.js
@@ -15,7 +15,6 @@ import Home from '../routes/Home';
 import Login from './Auth/Login';
 
 
-import Header from './Header/Header';
 import System from '../routes/System';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
@@ -48,7 +47,7 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        {this.props.isLoggedIn && <Header />}
+                        {/* {this.props.isLoggedIn && <Header />} */}
 
                         <div className="content-container">
                             <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
