875a219a7b4c8a2e7b66bccb591cfbef01b4c388
baothi <baothi246@gmail.com>
2023-03-28 22:10:09 +0700

    show all ok

--- nodejs_backend/src/config/connectDB.js
+++ nodejs_backend/src/config/connectDB.js
@@ -11,18 +11,10 @@ const { Sequelize } = require('sequelize');
 // });
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize(
-  process.env.DB_DATABASE_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-  logging: false,
-  query: {
-    "raw": true
-  },
-  timezone: "+07:00"
+const sequelize = new Sequelize('nodejs_backend', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  "logging": false
 });
 
 let connectDb = async () => {
